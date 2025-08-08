d339b54e7e4d2878832354547ceb04be080d5c4e
Hamza <hamzasepal@gmail.com>
2025-08-08 15:07:09 +0500

    sidebar added

--- src/components/admin/dashboard-header.tsx
+++ src/components/admin/dashboard-header.tsx
@@ -1,10 +1,12 @@
 import { Search, Bell, ChevronDown } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { SidebarTrigger } from "../ui/sidebar";
 
 export function DashboardHeader() {
   return (
     <div className="flex flex-row h-16 w-full  items-center justify-between gap-4 border-b border-gray-200 bg-white px-6">
+      <SidebarTrigger className="md:hidden" />
       <div className="w-1/2 flex items-center gap-4">
         <div className="relative">
           <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-400" />
